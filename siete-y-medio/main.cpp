fef232462cfad120af13384f0bcc1e391707e0ee
Valerie Polad <valeriepolad@gmail.com>
2018-04-20 23:35:34 -0700

    Initial Commit

---
+++ siete-y-medio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  siete-y-medio
+//
+//  Created by Valerie Polad on 4/20/18.
+//  Copyright © 2018 Valerie Polad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
